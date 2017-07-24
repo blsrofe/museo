805d7466cbacecd86d3a85c5b07418b97482c47f
Becki Srofe <blf_23@yahoo.com>
2017-07-24 09:23:14 -0600

    Add photo initialize method and tests

--- lib/photograph.rb
+++ lib/photograph.rb
@@ -1,2 +1,22 @@
 class Photograph
+
+  attr_reader :name,
+              :id,
+              :artist_id,
+              :museum_id,
+              :year
+
+  def initialize(hash)
+    @name = hash[:name]
+    @id = establish_id
+    @artist_id = hash[:artist_id]
+    @museum_id = hash[:museum_id]
+    @year = hash[:year]
+  end
+
+  def establish_id
+    id = 0
+    id += 1
+  end
+
 end
--- test/photograph_test.rb
+++ test/photograph_test.rb
@@ -1,7 +1,39 @@
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'minitest/emoji'
 require './lib/photograph'
 
 class PhotographTest < Minitest::Test
+
+  def test_it_exists
+    photograph = Photograph.new({name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
+    assert_instance_of Photograph, photograph
+  end
+
+  def test_it_has_id
+    photograph = Photograph.new({name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
+    assert_equal 1, photograph.id
+  end
+
+  def test_it_has_artist_id
+    photograph = Photograph.new({name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
+    assert_equal 4, photograph.artist_id
+  end
+
+  def test_it_has_museum_id
+    photograph = Photograph.new({name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
+    assert_equal 2, photograph.museum_id
+  end
+
+  def test_it_has_name
+    photograph = Photograph.new({name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
+    assert_equal "Rue Mouffetard, Paris (Boy with Bottles)", photograph.name
+  end
+
+  def test_it_has_a_year
+    photograph = Photograph.new({name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
+    assert_equal 1954, photograph.year
+  end
+
 end
